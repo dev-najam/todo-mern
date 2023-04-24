bfc29604a90c099cb12fb8a768f48db6801faa16
dev-najam <najamshahbaz3@gmail.com>
2023-04-24 19:25:32 +0500

    First Commit

---
+++ server/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+// const dotenv = require("dotenv");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// dotenv.config({ path: "./.env" });
+const PORT = process.env.PORT;
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    // useNewUrlPrser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("connection seccessful");
+  })
+  .catch((err) => {
+    console.log("no connection");
+  });
+
+const middleware = (req, res, next) => {
+  console.log(`middleware`);
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send(`hello world`);
+});
+
+app.get("/about", middleware, (req, res) => {
+  res.send(`About`);
+});
+
+app.get("/contact", (req, res) => {
+  res.send(`Contact`);
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`Sign in`);
+});
+
+app.get("/signup", (req, res) => {
+  res.send(`signup`);
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`server is running at ${PORT}`);
+});
